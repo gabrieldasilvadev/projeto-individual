23fbfc887cd9b396023cd866a19e936b9d90d3da
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-14 14:49:04 -0300

    Script swiper

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  speed: 400,
+  spaceBetween: 10,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
