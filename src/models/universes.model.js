499e6b8cf1a8e79f9035e9f1c61f826abe4a094a
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-15 21:52:39 -0300

    Created model universe

---
+++ src/models/universes.model.js
@@ -0,0 +1,18 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../database/db');
+const Team = require('./teams.model');
+
+const Universe = db.define('universo', {
+  idUniverso: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  nome: {
+    type: DataTypes.STRING(45),
+    allowNull: false,
+  },
+});
+
+module.exports = Universe;
