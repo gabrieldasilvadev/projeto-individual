eefe71659a337aa83e0278dc96ab605643b617a1
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-16 03:49:17 -0300

    Upload items router

--- src/routes/items.router.js
+++ src/routes/items.router.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const itemsController = require('../controllers/items.controller');
-
+const itemController = require('../controllers/items.controller');
 const itemsRouter = express.Router();
+
+itemsRouter.get('/items', itemController.getAllItems);
+itemsRouter.get('/items/:id', itemController.getItemById);
+itemsRouter.post('/items', itemController.createItem);
+itemsRouter.put('/items/:id', itemController.updateItem);
+itemsRouter.delete('/items/:id', itemController.deleteItem);
 
 module.exports = itemsRouter;
