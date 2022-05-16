1812b45eb8cb5cc813f357148d425ce64d8ea2ff
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-16 03:46:09 -0300

    require cors

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const usersRouter = require('./routes/users.router');
 const db = require('./database/db');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(usersRouter);
 
 db.sync({ alter: true });
-console.log(`Banco de dados conectado com sucesso: ${process.env.DB_NAME}`);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Servidor rodando na porta: http://localhost:${port}`)
+);
 
 module.exports = app;
