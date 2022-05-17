a7a38bcddb71a30c004a67609666e8561a59adc9
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-17 00:45:46 -0300

    Add routes posts on app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const usersRouter = require('./routes/users.router');
-const db = require('./database/db');
 const cors = require('cors');
+const db = require('./database/db');
+
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+
+app.use(express.static('public'));
 app.use(express.json());
 app.use(usersRouter);
+app.use(postsRouter);
 
-db.sync({ alter: true });
+db.sync();
 
 app.listen(port, () =>
   console.log(`Servidor rodando na porta: http://localhost:${port}`)
