129fe2e52bfddb03d0f13264ef3565407784d544
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-17 23:38:25 -0300

    import dependeces, session, file-store and flash

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('express-flash');
+
+const app = express();
+const port = 3000;
 const db = require('./database/db');
 
 const usersRouter = require('./routes/users.router');
 const postsRouter = require('./routes/posts.router');
 
-const app = express();
-const port = 3000;
 
 app.use(cors());
 
