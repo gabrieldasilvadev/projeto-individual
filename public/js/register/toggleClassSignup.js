0cf2bfd1ac590bf66d22008a646347b0faf4b400
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-18 00:06:38 -0300

    Change name file

--- public/js/register/toggleClassSignup.js
+++ public/js/register/toggleClassSignup.js
@@ -1,5 +1,3 @@
-document.querySelector('.img-btn').addEventListener('click', function()
-	{
-		document.querySelector('.cont').classList.toggle('s-signup')
-	}
-);
+document.querySelector('.img-btn').addEventListener('click', function () {
+  document.querySelector('.cont').classList.toggle('s-signup');
+});
