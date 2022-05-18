6b19e10097d30370d08d31a2af090ce9277d610a
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-18 00:08:08 -0300

    Configure session user

--- src/app.js
+++ src/app.js
@@ -2,23 +2,63 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 const flash = require('express-flash');
+const { tmpdir } = require('os');
+const FileStore = require('session-file-store')(session);
 
 const app = express();
 const port = 3000;
 const db = require('./database/db');
 
-const usersRouter = require('./routes/users.router');
-const postsRouter = require('./routes/posts.router');
+const usersRouter = require('./routes/user.router');
+const postsRouter = require('./routes/post.router');
 
 app.use(cors());
 
 app.use(express.static('public'));
 app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+app.use(
+  session({
+    name: 'session-id',
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: () => {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: ONE_DAY,
+      expires: new Date(Date.now() + ONE_DAY),
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
 app.use(usersRouter);
 app.use(postsRouter);
 
+// .sync({ force: true })
 db.sync()
   .then(
     app.listen(port, () =>
