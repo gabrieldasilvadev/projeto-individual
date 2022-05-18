0f163a7cfd7d182f478d21092f9aa84fcdbb0cba
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-18 01:01:41 -0300

    Create router for user auth

---
+++ src/routes/auth.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const authRouter = express.Router();
+const AuthController = require('../controllers/auth.controller');
+
+authRouter.get('/auth', AuthController.login);
+authRouter.get('/auth/register', AuthController.register);
+authRouter.get('/auth/register', AuthController.registerPost);
+
+module.exports = authRouter;
