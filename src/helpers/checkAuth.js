b9773626a05a4bdd6706e8b6e45954a0e1c5d72d
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-22 00:56:52 -0300

    Create function for check user authenticante

---
+++ src/helpers/checkAuth.js
@@ -0,0 +1,14 @@
+// create middleware to check if user is logged in
+
+const checkAuth = (req, res, next) => {
+  const userId = req.session?.userid;
+  console.log(req.session);
+  if (!userId) {
+    res.redirect('/auth');
+    return;
+  }
+
+  next();
+};
+
+module.exports = checkAuth;
