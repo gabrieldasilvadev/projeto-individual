a59175f8b59b73f08bf59e8882ae594bf87b1eae
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-22 00:57:17 -0300

    Change name routes auth

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const authRouter = express.Router();
 const AuthController = require('../controllers/auth.controller');
 
-authRouter.post('/login', AuthController.loginPost);
-authRouter.post('/register', AuthController.registerPost);
-authRouter.get('/list', AuthController.list);
-authRouter.get('/logout', AuthController.logout);
+authRouter.post('/api/login', AuthController.loginPost);
+authRouter.post('/api/register', AuthController.registerPost);
+authRouter.get('/api/list', AuthController.list);
+authRouter.get('/api/logout', AuthController.logout);
 
 module.exports = authRouter;
