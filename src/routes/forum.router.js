63d45347e7af9cebda7d96dfca43cff7c834fbd4
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-22 23:18:50 -0300

    Update forum router

--- src/routes/forum.router.js
+++ src/routes/forum.router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const postController = require('../controllers/forum.controller');
 const postRouter = express.Router();
-
 const checkAuth = require('../helpers/checkAuth');
 
 postRouter.get('/api/forum', checkAuth, postController.getAllPosts);
