6125ed5c16e78544c4506348e8c7d46915849dc7
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-22 23:19:05 -0300

    Comment console.log req.sessuib

--- src/helpers/checkAuth.js
+++ src/helpers/checkAuth.js
@@ -2,7 +2,7 @@
 
 const checkAuth = (req, res, next) => {
   const userId = req.session?.userid;
-  console.log(req.session);
+  // console.log(req.session);
   if (!userId) {
     res.redirect('/auth');
     return;
