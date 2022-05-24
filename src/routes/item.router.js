3db4cc7573e5c0048c140e0894c2748e8c827489
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-05-24 04:19:12 -0300

    Change routes name, add api before items

--- src/routes/item.router.js
+++ src/routes/item.router.js
@@ -2,10 +2,10 @@ const express = require('express');
 const itemController = require('../controllers/item.controller');
 const itemRouter = express.Router();
 
-itemRouter.get('/items', itemController.getAllItems);
-itemRouter.get('/items/:id', itemController.getItemById);
-itemRouter.post('/items', itemController.createItem);
-itemRouter.put('/items/:id', itemController.updateItem);
-itemRouter.delete('/items/:id', itemController.deleteItem);
+itemRouter.get('/api/items', itemController.getAllItems);
+itemRouter.get('/api/items/:id', itemController.getItemById);
+itemRouter.post('/api/items', itemController.createItem);
+itemRouter.put('/api/items/:id', itemController.updateItem);
+itemRouter.delete('/api/items/:id', itemController.deleteItem);
 
 module.exports = itemRouter;
