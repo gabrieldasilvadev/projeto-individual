cfd1d94386ff98be1d16c91515737fe8179ce29e
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-06-02 21:39:43 -0300

    Add backgrondColor when scrolling page

---
+++ public/js/header.js
@@ -0,0 +1,10 @@
+const header = document.getElementById('header');
+
+window.onscroll = () => {
+  window.pageYOffset > 200
+    ? (header.style.backgroundColor = 'var(--ui-black-02)') &&
+      (header.style.transition = 'all 0.4s ease-in-out') &&
+      (header.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.5)')
+    : (header.style.backgroundColor = 'transparent') &&
+      (header.style.boxShadow = 'none');
+};
