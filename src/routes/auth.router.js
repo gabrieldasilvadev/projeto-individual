42e0d50235b5dffa8afb0f81a78cf1e1863a76de
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-06-05 06:00:35 -0300

    Removed route api/list

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -4,7 +4,6 @@ const AuthController = require('../controllers/auth.controller');
 
 authRouter.post('/api/login', AuthController.loginPost);
 authRouter.post('/api/register', AuthController.registerPost);
-authRouter.get('/api/list', AuthController.list);
 authRouter.get('/api/logout', AuthController.logout);
 
 module.exports = authRouter;
