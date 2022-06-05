b097722c5a2dade24ac26c08e75641efb643455f
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-06-05 07:31:57 -0300

    Change pageYOffset 200 to 100

--- public/js/header.js
+++ public/js/header.js
@@ -1,7 +1,7 @@
 const header = document.getElementById('header');
 
 window.onscroll = () => {
-  window.pageYOffset > 200
+  window.pageYOffset > 100
     ? (header.style.backgroundColor = 'var(--ui-black-02)') &&
       (header.style.transition = 'all 0.4s ease-in-out') &&
       (header.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.5)')
