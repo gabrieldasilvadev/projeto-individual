20a26cf45f4d4c5aac11b5b49a757e07af1deea3
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-06-06 03:22:23 -0300

    Added routes upload Image and get Image

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -6,5 +6,7 @@ userRouter.get('/api/users', UserController.getAllUsers);
 userRouter.get('/api/users/:id', UserController.getUserById);
 userRouter.put('/api/users/:id', UserController.updateUser);
 userRouter.delete('/api/users/:id', UserController.deleteUser);
+userRouter.post('/upload-image', UserController.uploadImage);
+userRouter.get('/getImage', UserController.getImage);
 
 module.exports = userRouter;
