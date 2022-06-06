958f772fb2ceb0c1e18a9497c709eff12ce7cdff
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-06-06 03:22:52 -0300

    Created routes: dashboard, sobre, items, galeria and contato

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,10 +8,16 @@ indexRouter.use('/pages', checkAuth, (req, res) => {
   return;
 });
 indexRouter.use('/auth', express.static('public/pages/register-login.html'));
-
 indexRouter.use('/items', checkAuth, express.static('public/pages/items.html'));
-indexRouter.use('/dashboard', checkAuth, express.static('public/pages/dashboard.html'));
-
+indexRouter.use(
+  '/dashboard',
+  checkAuth,
+  express.static('public/pages/dashboard.html')
+);
+indexRouter.use('/sobre', express.static('public/pages/sobre.html'));
+indexRouter.use('/items', express.static('public/pages/items.html'));
+indexRouter.use('/galeria', express.static('public/pages/galeria.html'));
+indexRouter.use('/contato', express.static('public/pages/contato.html'));
 indexRouter.use(express.static('public'));
 
 module.exports = indexRouter;
